a3195880debad2f7045b3a786f85a0036e7960b0
takahito-shimada <shimada.takahito@d-publishing.jp>
2025-11-07 12:29:01 +0900

    モブ途中

--- training-weather/City.swift
+++ training-weather/City.swift
@@ -10,4 +10,25 @@ import Foundation
 struct City: Codable, Identifiable, Hashable {
     var id: Int
     var name: String
+    var weathers: [Weather]
 }
+
+
+struct Weather: Codable, Hashable, Identifiable {
+    var id:String
+    var date: String
+    var weather: weather
+    var image: String
+    
+    enum weather: String, CodingKey, CaseIterable {
+        case sun = "晴れ"
+        case cloud = "曇り"
+        case rain = "雨"
+        
+        var id: String {
+            rawValue
+        }
+    }
+}
+
+
